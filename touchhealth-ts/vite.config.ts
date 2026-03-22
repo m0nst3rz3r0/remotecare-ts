3c080333143bf6c0effea82cd2564a52bf13b4c6
m0nst3rz3r0 <alexalpha360@gmail.com>
2026-03-22 10:34:33 +0300

    Update vite.config.ts

--- touchhealth-ts/vite.config.ts
+++ touchhealth-ts/vite.config.ts
@@ -4,6 +4,7 @@ import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/',
   plugins: [react()],
   resolve: {
     alias: {
@@ -12,7 +13,7 @@ export default defineConfig({
   },
   build: {
     outDir: 'dist',
-    sourcemap: true,
+    sourcemap: false,
   },
   server: {
     port: 3000,
